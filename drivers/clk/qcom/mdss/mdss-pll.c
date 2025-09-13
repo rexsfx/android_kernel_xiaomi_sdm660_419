MotorolaMobilityLLC/kernel-msm

msm: mdss: add support for DP PLL driver on sdm630
Add support for DP PLL driver to bring up display port
on sdm630.

Change-Id: I075581be3c69841a7eb3909b28d5214728717f68
Signed-off-by: Narender Ankam <nankam@codeaurora.org>

drivers/clk/qcom/mdss/mdss-pll.c
@@ -149,6 +149,9 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 	} else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_sdm660")) {
 		pll_res->target_id = MDSS_PLL_TARGET_SDM660;
 		pll_res->pll_interface_type = MDSS_DP_PLL_SDM660;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_sdm630")) {
+		pll_res->target_id = MDSS_PLL_TARGET_SDM630;
+		pll_res->pll_interface_type = MDSS_DP_PLL_SDM630;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll_8996")) {
 		pll_res->pll_interface_type = MDSS_HDMI_PLL_8996;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll_8996_v2")) {
@@ -187,6 +190,9 @@ static int mdss_pll_clock_register(struct platform_device *pdev,
 		rc = dsi_pll_clock_register_14nm(pdev, pll_res);
 		break;
 	case MDSS_DP_PLL_SDM660:
+		rc = dp_pll_clock_register_14nm(pdev, pll_res);
+		break;
+	case MDSS_DP_PLL_SDM630:
 		rc = dp_pll_clock_register_14nm(pdev, pll_res);
 		break;
 	case MDSS_UNKNOWN_PLL:
@@ -396,6 +402,7 @@ static const struct of_device_id mdss_pll_dt_match[] = {
 	{.compatible = "qcom,mdss_dsi_pll_sdm660"},
 	{.compatible = "qcom,mdss_dp_pll_sdm660"},
 	{.compatible = "qcom,mdss_dsi_pll_sdm630"},
+	{.compatible = "qcom,mdss_dp_pll_sdm630"},
 	{}
 };
 

rexsfx/android_kernel_xiaomi_sdm660_419

drivers/clk/qcom/mdss/mdss-pll.c
@@ -124,13 +124,17 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 		pll_res->pll_interface_type = MDSS_DSI_PLL_14NM;
 		pll_res->target_id = MDSS_PLL_TARGET_SDM660;
 		pll_res->revision = 2;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_sdm660")) {
+		pll_res->pll_interface_type = MDSS_DP_PLL_14NM;
+		pll_res->target_id = MDSS_PLL_TARGET_SDM660;
+		pll_res->revision = 2;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_sdm630")) {
 		pll_res->pll_interface_type = MDSS_DSI_PLL_14NM;
 		pll_res->target_id = MDSS_PLL_TARGET_SDM630;
 		pll_res->revision = 2;
-	} else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_sdm660")) {
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_sdm630")) {
 		pll_res->pll_interface_type = MDSS_DP_PLL_14NM;
-		pll_res->target_id = MDSS_PLL_TARGET_SDM660;
+		pll_res->target_id = MDSS_PLL_TARGET_SDM630;
 		pll_res->revision = 2;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_12nm"))
 		pll_res->pll_interface_type = MDSS_DSI_PLL_12NM;
@@ -403,6 +407,7 @@ static const struct of_device_id mdss_pll_dt_match[] = {
 	{.compatible = "qcom,mdss_dsi_pll_12nm"},
 	{.compatible = "qcom,mdss_dsi_pll_28lpm"},
 	{.compatible = "qcom,mdss_dsi_pll_sdm630"},
+	{.compatible = "qcom,mdss_dp_pll_sdm630"},
 	{}
 };
 